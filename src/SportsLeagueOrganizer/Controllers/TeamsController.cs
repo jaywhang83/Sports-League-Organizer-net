b8168ce8fab779068c40752392e3dfccd8477f6e
Jay <jaywhang83@gmail.com>
2016-04-19 14:54:18 -0700

    add all crud functionality and styling

--- src/SportsLeagueOrganizer/Controllers/TeamsController.cs
+++ src/SportsLeagueOrganizer/Controllers/TeamsController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Linq;
 using Microsoft.AspNet.Mvc;
 using SportsLeagueOrganizer.Models;
-
+using Microsoft.Data.Entity;
 
 namespace SportsLeagueOrganizer.Controllers
 {
@@ -18,6 +18,51 @@ namespace SportsLeagueOrganizer.Controllers
         {
             var thisTeam = db.Teams.FirstOrDefault(x => x.TeamId == id);
             return View(thisTeam);
+        }
+
+        //New Team
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Team team)
+        {
+            db.Teams.Add(team);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Edit Team
+        public IActionResult Edit(int id)
+        {
+            var thisTeam = db.Teams.FirstOrDefault(x => x.TeamId == id);
+            return View(thisTeam);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Team team)
+        {
+            db.Entry(team).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Delete Team
+        public IActionResult Delete(int id)
+        {
+            var thisTeam = db.Teams.FirstOrDefault(x => x.TeamId == id);
+            return View(thisTeam);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisTeam = db.Teams.FirstOrDefault(x => x.TeamId == id);
+            db.Teams.Remove(thisTeam);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- src/SportsLeagueOrganizer/Startup.cs
+++ src/SportsLeagueOrganizer/Startup.cs
@@ -38,6 +38,8 @@ namespace SportsLeagueOrganizer
         {
             app.UseIISPlatformHandler();
 
+            app.UseStaticFiles();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
